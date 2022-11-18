6484d8dfecdda6f6301e16af7a1108d3f9037436
Ivan Oparin <ivandmit@gmail.com>
2022-11-18 16:54:59 -0500

    plotted emissivity

--- xray_imaging.py
+++ xray_imaging.py
@@ -3,17 +3,31 @@ import yt
 yt.enable_parallelism() # run in console as  mpirun -n 6 python3 xray_imaging.py
 import pyxsim
 #import soxs
+units_override = {
+    "length_unit": (1.5e8, "m"),
+    "time_unit": (109.8, "s"),
+    "mass_unit": (8.43e38, "kg"),
+    #"density_unit": (2.5e14, "kg/m**3"),
+    "velocity_unit": (1.366e6, "m/s"),
+    "temperature_unit": (1.13e8, "K"),
+}
 
-ds = yt.load("datacubes/flarecs-id.0035.vtk", default_species_fields='ionized')
+ds = yt.load("datacubes/flarecs-id.0035.vtk", units_override=units_override, default_species_fields='ionized')
 ds.field_list
+print("done")
+#%%
 #source_model = pyxsim.CIESourceModel("apec")#, emin, emax, nbins, Zmet)
 #thermal_model = pyxsim.CIESourceModel("apec", 0.1, 11.0, 10000, 0.3)
-thermal_model = pyxsim.CIESourceModel("apec", 0.025, 64.0, 1000, 1, binscale='log')
+thermal_model = pyxsim.CIESourceModel("apec", 0.025, 64.0, 100, 0.3, binscale='log')
 #%%
-#slc = yt.SlicePlot(ds, 'z', [('athena', 'density'), ('athena', 'total_energy')], width=(0.75, 1))
-#slc.save()
+slc = yt.SlicePlot(ds, 'z', [('athena', 'density'), ('athena', 'total_energy')], width=(0.75, 1))
+slc.save()
 #%%
 #source_model = pyxsim.CIESourceModel("spex", 0.05, 11.0, 1000, 0.3, binscale='log')
+'''
+Cut different regions in yt:
+https://yt-project.org/doc/analyzing/objects.html
+'''
 #%%
 xray_fields = thermal_model.make_source_fields(ds, 0.5, 64.0)
 print(xray_fields)
