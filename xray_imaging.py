9734a5268261676e6cbb0892bd080de8e42ee5aa
Ivan Oparin <ivandmit@gmail.com>
2022-12-12 18:55:54 -0500

    Intensity maps+projections and mpirun arguments updated

--- xray_imaging.py
+++ xray_imaging.py
@@ -1,6 +1,7 @@
 import yt
 #yt.toggle_interactivity()
-#yt.enable_parallelism() # run in console as  mpirun -n 6 python3 xray_imaging.py
+yt.enable_parallelism() # run in console as  mpirun -n 6 python3 xray_imaging.py
+# to run on all processor threads: mpirun --use-hwthread-cpus python3 xray_imaging.py
 import pyxsim
 #import soxs
 units_override = {
@@ -21,25 +22,37 @@ print("done")
 #thermal_model = pyxsim.CIESourceModel("apec", 0.1, 11.0, 10000, 0.3)
 thermal_model = pyxsim.CIESourceModel("apec", 0.025, 64.0, 100, 0.3, binscale='log')
 #%%
-slc = yt.SlicePlot(ds, 'z', [('athena', 'density'), ('athena', 'total_energy')], width=(0.75, 1))
+# slc = yt.SlicePlot(ds, 'z', [('athena', 'density'), ('athena', 'total_energy')], width=(0.75, 1))
 #%%
-slc.save()
+# slc.save()
 #%%
 #source_model = pyxsim.CIESourceModel("spex", 0.05, 11.0, 1000, 0.3, binscale='log')
 '''
 Cut different regions in yt:
 https://yt-project.org/doc/analyzing/objects.html
+Find the path of source pyxsim codes:
+/home/ivan/anaconda3/envs/radcube/lib/python3.9/site-packages/pyxsim/__init__.py
 '''
+# #%%
+# xray_fields = thermal_model.make_source_fields(ds, 0.5, 64.0)
+# print(xray_fields)
+# print('done')
+# #%%
+# #print(ds["gas","xray_luminosity_0.5_7.0_keV"])
+# #print(ds.sum(("gas","xray_luminosity_0.5_7.0_keV")))
+# #%%
+# print('projecting')
+# prj = yt.ProjectionPlot(ds, "z", ("gas", "xray_emissivity_0.5_64.0_keV"), width=(0.75, 1))
+# prj.save()
 #%%
-xray_fields = thermal_model.make_source_fields(ds, 0.5, 64.0)
+'''
+Try to plot an intensity map here
+'''
+xray_fields = thermal_model.make_intensity_fields(ds, 0.5, 64.0, dist=(1.5e11, "m"))
 print(xray_fields)
 print('done')
 #%%
-#print(ds["gas","xray_luminosity_0.5_7.0_keV"])
-#print(ds.sum(("gas","xray_luminosity_0.5_7.0_keV")))
-#%%
-print('projecting')
-prj = yt.ProjectionPlot(ds, "z", ("gas", "xray_emissivity_0.5_64.0_keV"), width=(0.75, 1))
+# Project and save image
+prj = yt.OffAxisProjectionPlot(ds, [0.0, 0.0, 1.0], xray_fields[-1],
+                           width=(1.5e8, "m"), north_vector=[-0.8, 0.6, 0.0])
 prj.save()
-
-print('done')
