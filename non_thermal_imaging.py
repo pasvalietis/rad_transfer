174db32f852e66331677301325cb2fc4dfa963ea
Ivan Oparin <ivandmit@gmail.com>
2023-02-03 18:10:21 -0500

    Try to access different fields

--- non_thermal_imaging.py
+++ non_thermal_imaging.py
@@ -38,11 +38,54 @@ ds.add_field(
 )
 
 #npp, npc = pyxsim.make_photons("plaw_photons", sp, redshift, A, exp_time, plaw_model)
+#%%
 
-xray_fields = plaw_model.make_intensity_fields(ds, 20.0, 30.0, dist=(3, "mpc"))
-print(xray_fields)
+emin = 20.0
+emax = 30.0
 
-prj = yt.ProjectionPlot(
-    ds, "z", ("gas", "xray_photon_intensity_20.0_30.0_keV"), width=(1.0, "Mpc")
+from pyxsim.utils import ParallelProgressBar, parse_value
+
+emin = parse_value(emin, "keV")
+emax = parse_value(emax, "keV")
+
+emin_src = emin * (1.0)
+emax_src = emax * (1.0)
+
+#xray_fields = plaw_model.make_intensity_fields(ds, emin, emax, dist=(3, "mpc"))
+#print(xray_fields)
+#%%
+#prj = yt.ProjectionPlot(
+#    ds, "z", ("gas", "xray_photon_intensity_20.0_30.0_keV"), width=(1.0, "Mpc")
+#)
+#prj.save()
+
+#%%
+
+ftype = 'gas'#plaw_model.emission_field[0]
+force_override = False
+# dist_fac, redshift = plaw_model._make_dist_fac(
+#             ds, redshift, dist, cosmology, per_sa=True
+#         )
+
+eif = plaw_model.make_fluxf(emin_src, emax_src, energy=True)
+
+def _intensity_field(field, data):
+    ret = data.ds.arr(
+        plaw_model.process_data("energy_field", data, 1.0, fluxf=eif),
+        "keV/s",
+    )
+    idV = data[ftype, "density"] / data[ftype, "mass"]
+    I = 1 * ret * idV
+    return I.in_units("erg/cm**3/s/arcsec**2")
+
+ei_name = (ftype, f"xray_intensity_{emin.value}_{emax.value}_keV")
+ei_dname = rf"I_{{X}} ({emin.value}-{emax.value} keV)"
+
+ds.add_field(
+    ei_name,
+    function=_intensity_field,
+    display_name=ei_dname,
+    sampling_type="local",
+    units="erg/cm**3/s/arcsec**2",
+    force_override=force_override,
 )
-prj.save()
