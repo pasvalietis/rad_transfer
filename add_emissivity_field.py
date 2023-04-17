ebf73e1dad19a2005ded3e011d93bde5f2443bf5
Ivan Oparin <ivandmit@gmail.com>
2023-04-17 19:31:27 -0400

    Test the full dataset parameters

--- add_emissivity_field.py
+++ add_emissivity_field.py
@@ -1,6 +1,8 @@
 import yt
 yt.enable_parallelism()
 import numpy as np
+import pickle
+from pathlib import Path
 from yt.data_objects.static_output import Dataset
 #%%
 
@@ -20,6 +22,16 @@ class ThermalBremsstrahlungModel:
         self.density_field = ds._get_field_info(self.density_field).name
         self.emission_field = ds._get_field_info(self.cell_mass).name
         self.ftype = self.temperature_field[0]
+
+    # def do_something_with(data):
+    #     data = np.zeros(10000000)
+    #     output = np.zeros_like(data)
+    #     chunk_size = 1024
+    #     for i_chunk in range(int(ceil(len(data) / chunk_size))):
+    #         data_chunk = data[i_chunk * chunk_size:(i_chunk + 1) * chunk_size]
+    #         output_chunk = process_data(data_chunk)
+    #         output[i_chunk * chunk_size:(i_chunk + 1) * chunk_size] = output_chunk
+    #     return output
 
     def process_data(self, chunk):
         norm = chunk[self.emission_field].d
@@ -57,40 +69,51 @@ class ThermalBremsstrahlungModel:
             units="photons/cm**3/s/arcsec**2",
         )
 
-
 path_test = "datacubes/id0/Blast.0020.vtk"
 path_default = "datacubes/flarecs-id.0035.vtk"
 #path_subs = "datacubes/flarecs-id.0035_ss3.h5"
 
-# L_0 = (1.5e8, "m")
-# units_override = {
-#     "length_unit": L_0,
-#     "time_unit": (109.8, "s"),
-#     "mass_unit": (8.4375e38, "g"),  # m_0 = \rho_0 * (L_0 ** 3)
-#     # "density_unit": (2.5e14, "kg/m**3"),
-#     "velocity_unit": (1.366e6, "m/s"),
-#     "temperature_unit": (1.13e8, "K"),
-# }
+L_0 = (1.5e8, "m")
+units_override = {
+    "length_unit": L_0,
+    "time_unit": (109.8, "s"),
+    "mass_unit": (8.4375e38, "g"),  # m_0 = \rho_0 * (L_0 ** 3)
+    # "density_unit": (2.5e14, "kg/m**3"),
+    "velocity_unit": (1.366e6, "m/s"),
+    "temperature_unit": (1.13e8, "K"),
+}
 
-'''
 # Add temperature field to a subsampled dataset // Talk about it to Sabastian 
 
 def _temperature(field, data):
     pc = data.ds.units.physical_constants
-    return (data.ds.mu * data["gas", "pressure"] / data["gas", "density"] * pc.mh / pc.kboltz)
+    return (data.ds.mu * data["gas", "pressure"] / data["gas", "density"] * pc.mh / pc.kboltz).in_units("K")
     
-ds.add_field(
-("gas", "temperature"),
-function=_temperature,
-sampling_type="cell",
-units="K",
-)
-'''
+# ds.add_field(
+# ("gas", "temperature"),
+# function=_temperature,
+# sampling_type="cell",
+# units="K",
+# )
 
-ds = yt.load(path_test, default_species_fields='ionized') #, units_override=units_override) #, default_species_fields='ionized')
+# path_default_pickle = path_default + ".pickle"
+# if Path(path_default_pickle).exists():
+#     with open(path_default_pickle, 'rb') as file:
+#         ds = pickle.load(file)
+# else:
+ds = yt.load(path_default, units_override=units_override, default_species_fields='ionized')
+    # with open(path_default_pickle, 'wb') as file:
+    #     pickle.dump(ds, file)  # dump data to f
+
 
 #%%
 thermal_model = ThermalBremsstrahlungModel("temperature", "density", "cell_mass")
+
+n_bins = 1000
+hist, bins = np.histogram(
+    ds.all_data()[('gas', 'temperature')],
+    bins=n_bins,
+)
 
 thermal_model.make_intensity_fields(ds)
 print(ds.all_data()['gas', 'xray_intensity_keV'])
@@ -116,6 +139,6 @@ To be described later
 #     units="erg/cm**3/s/arcsec**2",
 #     force_override=force_override,
 # )
-
 # print(ds.all_data()['gas', 'xray_intensity_keV'])
 
+#%%
