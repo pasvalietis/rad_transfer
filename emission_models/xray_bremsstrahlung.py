8bb8bf946ca23b706a2a9a0660704f880f1e0210
Ivan Oparin <ivandmit@gmail.com>
2023-06-02 21:01:21 -0400

    Integrating over energies to find flux in a given energy range

--- emission_models/xray_bremsstrahlung.py
+++ emission_models/xray_bremsstrahlung.py
@@ -6,6 +6,8 @@ from yt.data_objects.static_output import Dataset
 import math
 
 from scipy.special import k0
+from scipy.integrate import quad
+
 from astropy import constants as const
 from astropy import units as u
 from yt.fields.particle_fields import obtain_relative_velocity_vector
@@ -42,10 +44,15 @@ class ThermalBremsstrahlungModel:
     #         output[i_chunk * chunk_size:(i_chunk + 1) * chunk_size] = output_chunk
     #     return output
 
-    def process_data(self, chunk):
+    def process_data(self, chunk, spec_param):
 
         kboltz = 1.3807e-16  # Boltzmann's constant
         hplanck = 6.6261e-27  # Planck's constant cgs
+
+        emin = spec_param["emin"]
+        emax = spec_param["emax"]
+
+        nbins = 100
 
         photon_energy = 6. * u.keV
         photon_energy_erg = photon_energy.to(u.erg)
@@ -61,6 +68,10 @@ class ThermalBremsstrahlungModel:
         idV = mass / dens
         #for i in range(3):
         #idV *= ((self.L_0 / self.domain_dimensions[i]) * sizes[i])
+
+        dA = 1.
+        for i in range(2):
+            dA *= ((self.L_0 / self.domain_dimensions[i]) * sizes[i])
 
         # idV = 4.74e17 # mass / dens
         #gaunt = 1.5 #np.exp(0.5 * norm_energy) * k0(0.5 * norm_energy)
@@ -84,23 +95,40 @@ class ThermalBremsstrahlungModel:
         #em_data *= 1./(hplanck) # to get flux in photons/(s cm^2 keV)
         #em_data *= 1./(au_dist**2.)
 
+        def integrand(dens, temp, energy):
+            intd = 8.1e-39 * np.exp(-np.abs((energy*u.keV).to(u.erg).value / temp*kboltz))
+            intd *= (dens * dens) / np.sqrt(temp)
+            #intd *= np.exp(-energy)
+            return intd
+
+        def energy_int(dens, temp, emin, emax):
+            return quad(lambda energy: integrand(dens, temp, energy), emin, emax)
+
+        vect_eint = np.vectorize(energy_int)
+        res = vect_eint(dens, temp, emin, emax)[0]
+        em_data = res
+
         # TEST
-        em_data = 8.1e-39 * np.exp(-np.abs(norm_energy)) * ((dens**2.)/np.sqrt(np.abs(temp))) * idV
-        #angular_factor = 1. / (4. * np.pi * ((u.rad).to(u.arcsec)) ** 2.)
-        #em_data *= angular_factor / photon_energy_erg.value
-        #print('num cells', num_cells)
-        #ncells = 0
+        # em_data = 8.1e-39 * np.exp(-np.abs(norm_energy)) * ((dens**2.)/np.sqrt(np.abs(temp))) * dA #* idV
+        # angular_factor = 1. / (4. * np.pi * ((u.rad).to(u.arcsec)) ** 2.)
+        # em_data *= angular_factor / photon_energy_erg.value
+        # print('num cells', num_cells)
+        # ncells = 0
         return em_data
 
-    def make_intensity_fields(self, ds):
+    def make_intensity_fields(self, ds, emin, emax):
         self.setup_model(ds)
         ftype = self.ftype
         ei_name = (ftype, f"xray_intensity_keV")
         ei_dname = rf"I_{{X}} (keV)"
 
+        emin = emin
+        emax = emax
+        spec_param = {"emin": emin, "emax": emax}
+
         def _intensity_field(field, data):
             ret = data.ds.arr(
-                self.process_data(data),
+                self.process_data(data, spec_param=spec_param),
                 "photons/cm**3/s/arcsec**2",
             )
             return ret
