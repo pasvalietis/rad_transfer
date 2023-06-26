6c956cf2359d50cba3fed65484fd180f9b33abeb
Ivan Oparin <ivandmit@gmail.com>
2023-06-26 14:50:50 -0400

    Plotting Hinode images with corresponding colormaps

--- tests/rad_cls.py
+++ tests/rad_cls.py
@@ -2,6 +2,7 @@ import yt
 from buffer import RadDataset  # How to solve yt.utilities.exceptions.YTAmbiguousDataType error?
 from buffer import downsample
 import os.path
+import sys
 from emission_models import xray_bremsstrahlung
 from emission_models import uv
 
@@ -10,7 +11,11 @@ import matplotlib.colors as colors
 import matplotlib
 import numpy as np
 
-import sunpy.visualization.colormaps as cm
+# import sunpy.visualization.colormaps as cm
+
+sys.path.insert(0, '/home/ivan/Study/Astro/solar')
+from rad_transfer.emission_models import xrt
+from rad_transfer.visualization.colormaps import color_tables
 
 original_file_path = '../datacubes/flarecs-id.0035.vtk'
 
@@ -33,33 +38,39 @@ if not os.path.isfile(downs_file_path):
 else:
     rad_buffer_obj = yt.load(downs_file_path, hint="YTGridDataset")
 
+cut_box = rad_buffer_obj.region(center=[0.0, 0.5, 0.0],
+                                left_edge=[-0.5, 0.016, -0.25], right_edge=[0.5, 1.0, 0.25])
 # Create x_ray fields and produce an image
 #%%
-thermal_model = xray_bremsstrahlung.ThermalBremsstrahlungModel("temperature", "density", "mass")
-thermal_model.make_intensity_fields(rad_buffer_obj)
+# thermal_model = xray_bremsstrahlung.ThermalBremsstrahlungModel("temperature", "density", "mass")
+# thermal_model.make_intensity_fields(rad_buffer_obj)
 
 #%%
-channel = 'A94'
-sdo_aia_model = uv.UVModel("temperature", "density", channel)
-sdo_aia_model.make_intensity_fields(rad_buffer_obj)
+channel = 'Be-thin'
+hinode_xrt_model = xrt.XRTModel("temperature", "density", channel)
+hinode_xrt_model.make_intensity_fields(rad_buffer_obj)
 #%%
 # Use sunpy AIA colormaps
-sdoaia94 = matplotlib.colormaps['sdoaia94']
-sdoaia171 = matplotlib.colormaps['sdoaia171']
-sdoaia131 = matplotlib.colormaps['sdoaia131']
-sdoaia335 = matplotlib.colormaps['sdoaia335']
+# sdoaia94 = matplotlib.colormaps['sdoaia94']
+# sdoaia171 = matplotlib.colormaps['sdoaia171']
+# sdoaia131 = matplotlib.colormaps['sdoaia131']
+# sdoaia335 = matplotlib.colormaps['sdoaia335']
+
+xrt_colormap = color_tables.xrt_color_table()
 #%%
 N = 512
-nframes = 25
+nframes = 40
+plt.ioff()
+
 for i in range(nframes):
     norm_vec = [1.0 - i*(1./nframes), 0.0+i*(1./(nframes*3.)), i*(1./nframes)]
     prji = yt.visualization.volume_rendering.off_axis_projection.off_axis_projection(
-                            rad_buffer_obj,
+                            cut_box,
                             [0.0, 0.5, 0.0],  # center position in code units
                             norm_vec,  # normal vector (z axis)
                             1.0,  # width in code units
                             N,  # image resolution
-                            'aia_filter_band',  # respective field that is being projected
+                            'xrt_filter_band',  # respective field that is being projected
                             north_vector=[0.0, 1.0, 0.0])
 
     Mm_len = 1 # ds.length_unit.to('Mm').value
@@ -72,15 +83,15 @@ for i in range(nframes):
     data_img = np.array(prji)
     imag = data_img #+ 1e-17*np.ones((N, N))  # Eliminate zeros in logscale
 
-    vmin=0.8
-    vmax=80
+    vmin=2
+    vmax=2e3
     imag[imag == 0] = vmin
 
     pcm = ax.pcolor(X, Y, imag,
                             norm=colors.LogNorm(vmin=vmin, vmax=vmax),
                             #vmin=1e-5,
                             #vmax=1.5e4,
-                            cmap=sdoaia94, shading='auto')
+                            cmap=xrt_colormap, shading='auto')
     int_units = str(prji.units)
     #fig.colorbar(pcm, ax=ax, extend='max', label='$'+int_units.replace("**", "^")+'$')
     fig.colorbar(pcm, ax=ax, extend='max', label='DN/pixel * '+'$'+int_units.replace("**", "^")+'$')
@@ -90,7 +101,7 @@ for i in range(nframes):
     #figpath = '../img/rad_tr_thermal_brem/'
     #plt.savefig(figpath + 'therm_brem_front_view_rad_buff.png')
 
-    ax.set_title('AIA '+channel[1:]+' Å')
+    #ax.set_title('AIA '+channel[1:]+' Å')
     #plt.show()
-    figpath = '../img/rad_tr_sdo_aia/mov_'+channel+'/'
-    plt.savefig(figpath + 'sdo_aia_'+channel+'_mov_'+str(i).zfill(3)+'.png')
+    figpath = '../img/rad_hinode/'
+    plt.savefig(figpath + 'hinode_'+channel+'_mov_'+str(i).zfill(3)+'.png')
