a03ea91cbee721fcfd00ebe08fef9dbca66a0b04
Ivan Oparin <ivandmit@gmail.com>
2023-07-11 19:18:10 -0400

    Created class SyntheticFilterImage to plot and project images from different instruments

--- utils/proj_imag.py
+++ utils/proj_imag.py
@@ -10,94 +10,82 @@ import matplotlib.colors as colors
 from emission_models import uv, xrt
 from visualization.colormaps import color_tables
 
-def make_filter_image(
-        ds,
-        instr,
-        channel,
-        figpath=os.getcwd(),
-        norm_vec=(0.0, 0.0, 1.0),  # pass vectors as mutable arguments
-        north_vector=(-0.7, -0.3, 0.0),
-        resolution=512,
-        vmin=1e-15,
-        vmax=1e6,
-        logscale=True,
-        frame=None,
-        label=None):
+class SyntheticFilterImage:
+    """
+    Load a yt readable dataset and plot synthetic image having given the instrument name and wavelength
+    """
 
-    cmap = {}
-    imaging_model = None
+    def __init__(self, dataset, instr, channel, hint=None, units_override=None,
+                 view_settings=None, plot_settings=None, **kwargs):
+        """
+        :param dataset: Path of the downsampled dataset or a dataset itself
+        """
+        if isinstance(dataset, str):
+            self.data = yt.load(dataset, units_override=units_override, hint=hint)
+        elif isinstance(dataset, yt.data_objects.static_output.Dataset):
+            self.data = dataset
 
-    if instr == 'xrt':
-        imaging_model = xrt.XRTModel("temperature", "density", channel)
-        cmap['xrt'] = color_tables.xrt_color_table()
-    if instr == 'aia':
-        imaging_model = uv.UVModel("temperature", "density", channel)
-        try:
-            cmap['aia'] = color_tables.aia_color_table(int(channel)*u.angstrom)
-        except ValueError:
-            raise ValueError("AIA wavelength should be one of the following:"
-                             "1600, 1700, 4500, 94, 131, 171, 193, 211, 304, 335.")
+        self.instr = instr
+        self.channel = channel
+        self.view_settings = {'normal_vector': (0.0, 0.0, 1.0),  # pass vectors as mutable arguments
+                              'north_vector': (-0.7, -0.3, 0.0)}
+        self.__imag_field = None
+        self.image = None
 
-    imaging_model.make_intensity_fields(ds)
-    field = str(instr) + '_filter_band'
+        self.plot_settings = {'resolution': 512,
+                              'vmin': 1e-15,
+                              'vmax': 1e6,
+                              'cmap': 'inferno',
+                              'logscale': True,
+                              'figpath': './prj_plt.png',
+                              'frame': None,
+                              'label': None}
 
-    proj_and_imag(ds, field, list(norm_vec), list(north_vector), resolution, vmin, vmax,
-                  cmap[instr], logscale, figpath, frame, label)
-    return
+    def make_filter_image_field(self, **kwargs):
 
+        cmap = {}
+        imaging_model = None
+        instr_list = ['xrt', 'aia']
 
-def proj_and_imag(data, field, norm_vec=[0.0, 0.0, 1.0], north_vector=[-0.7, -0.3, 0.0], resolution=512,
-                  vmin=1e-15, vmax=1e6, cmap='inferno', logscale=True, figpath='./prj_plt.png', frame=None, label=None):
-    plt.ioff()
+        if self.instr not in instr_list:
+            raise ValueError("instr should be in the instrument list: ", instr_list)
 
-    prji = yt.visualization.volume_rendering.off_axis_projection.off_axis_projection(
-        data,
-        [0.1, 0.5, 0.0],  # center position in code units
-        norm_vec,  # normal vector (z axis)
-        1.0,  # width in code units
-        resolution,  # image resolution
-        field,  # respective field that is being projected
-        north_vector=north_vector)
+        if self.instr == 'xrt':
+            imaging_model = xrt.XRTModel("temperature", "density", self.channel)
+            cmap['xrt'] = color_tables.xrt_color_table()
+        if self.instr == 'aia':
+            imaging_model = uv.UVModel("temperature", "density", self.channel)
+            try:
+                cmap['aia'] = color_tables.aia_color_table(int(self.channel) * u.angstrom)
+            except ValueError:
+                raise ValueError("AIA wavelength should be one of the following:"
+                                 "1600, 1700, 4500, 94, 131, 171, 193, 211, 304, 335.")
 
-    Mm_len = 1  # ds.length_unit.to('Mm').value
-    X, Y = np.mgrid[-0.5 * 150 * Mm_len:0.5 * 150 * Mm_len:complex(0, resolution),
-           0 * Mm_len:150 * Mm_len:complex(0, resolution)]
-    fig, ax = plt.subplots()
-    data_img = np.array(prji)
+        imaging_model.make_intensity_fields(self.data)
 
-    imag = data_img
-    if field == 'velocity_divergence':
-        imag = np.abs(imag)
+        field = str(self.instr) + '_filter_band'
+        self.__imag_field = field
 
-    imag[imag == 0] = vmin
+        if self.plot_settings:
+            self.plot_settings['cmap'] = cmap[self.instr]
 
-    if logscale:
-        pcm = ax.pcolor(X, Y, imag, norm=colors.LogNorm(vmin=vmin, vmax=vmax), cmap=cmap, shading='auto')
-    else:
-        pcm = ax.pcolor(X, Y, imag, vmin=vmin, vmax=vmax, cmap=cmap, shading='auto')
+    def proj_and_imag(self, plot_settings=None, view_settings=None, **kwargs):
 
-    int_units = str(prji.units)
+        self.make_filter_image_field()  # Create emission fields
 
-    if 'xray' in field:
-        cbar_label = '$'+int_units.replace("**", "^")+'$'
-    elif 'DN' or 'uv' in field:
-        cbar_label = 'DN/pixel * '+'$'+int_units.replace("**", "^")+'$'
+        if plot_settings:
+            self.plot_settings.update(plot_settings)
 
-    fig.colorbar(pcm, ax=ax, extend='max', label=cbar_label)
-    ax.set_xlabel('x, Mm')
-    ax.set_ylabel('y, Mm')
+        if view_settings:
+            self.view_settings.update(view_settings)
 
-    if label != None:
-        plt.text(40, 135, label, color='white') #bbox=dict(fill=False, color='white', linewidth=0.0))
-    # figpath = '../img/rad_tr_thermal_brem/'
-    if frame == None:
-        if label != None:
-            plt.savefig(figpath + field + '_' + label + '.png')
-        else:
-            plt.savefig(figpath)
-            plt.close()
-            print('fig saved at: ', figpath)
-    else:
-        plt.savefig(figpath + field + '_' + str(frame) + '.png')
+        prji = yt.visualization.volume_rendering.off_axis_projection.off_axis_projection(
+            self.data,
+            [0.1, 0.5, 0.0],  # center position in code units
+            normal_vector=list(self.view_settings['normal_vector']),  # normal vector (z axis)
+            width=self.data.domain_width[0].value,  # width in code units
+            resolution=self.plot_settings['resolution'],  # image resolution
+            item=self.__imag_field,  # respective field that is being projected
+            north_vector=list(self.view_settings['north_vector']))
 
-    return imag
+        self.image = np.array(prji)
