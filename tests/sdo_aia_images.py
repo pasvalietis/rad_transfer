3a6cd731dc1f7504a506eea87899b59e449d65f5
Ivan Oparin <ivandmit@gmail.com>
2023-07-26 21:51:29 -0400

    Fix imshow/pcolormesh inconsistency, plotted sunpy map for AIA image

--- tests/sdo_aia_images.py
+++ tests/sdo_aia_images.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 import matplotlib.colors as colors
 import matplotlib
 import scipy.ndimage
@@ -13,24 +14,27 @@ from scipy.ndimage import zoom
 import math
 from visualization.colormaps import color_tables
 
-
-
 import sunpy.map
 import sunpy.visualization.colormaps as cm
 #from sunpy.data.sample import AIA_131_IMAGE
+
 
 import yt
 import os, sys
 
 sys.path.insert(0, '/home/ivan/Study/Astro/solar')
 from rad_transfer.utils.proj_imag import SyntheticFilterImage as synt_img
+from rad_transfer.emission_models import uv, xrt
 
+asec2cm = 0.725e8  # converting from arcsec to cm
 
 aia_imag = './aia_data/aia.lev1.131A_2011-03-07T180909.62Z.image_lev1.fits'
 aia_map = sunpy.map.Map(aia_imag)
 
 roi_bottom_left = SkyCoord(Tx=-500*u.arcsec, Ty=150*u.arcsec, frame=aia_map.coordinate_frame)
+width_Mm = (np.abs(-500+150)*asec2cm*u.cm).to(u.Mm).value
 roi_top_right = SkyCoord(Tx=-150*u.arcsec, Ty=500*u.arcsec, frame=aia_map.coordinate_frame)
+height_Mm =  (np.abs(500-150)*asec2cm*u.cm).to(u.Mm).value
 cusp_submap = aia_map.submap(roi_bottom_left, top_right=roi_top_right)
 
 # fig = plt.figure()
@@ -42,6 +46,8 @@ cusp_submap = aia_map.submap(roi_bottom_left, top_right=roi_top_right)
 #
 # #plt.show()
 # plt.close()
+
+#TODO: Add method to pass slit location using coordinates
 
 class ProfilePlot():
 
@@ -55,6 +61,10 @@ class ProfilePlot():
                np.linspace(self.pix1[1], self.pix2[1], self.num)
 
         self.profile = scipy.ndimage.map_coordinates(self.imag, np.vstack((x, y)))
+
+    def asec_to_pix(self):
+        pix = imag
+        return pix
 
 # def cut_across_imag(pix1, pix2, res, imag):
 #     x0, y0 = pix1[0], pix1[1]  # These are in _pixel_ coordinates!!
@@ -70,7 +80,7 @@ Examine AIA brightness distribution along the line
 '''
 
 # Extract the values along the line, using cubic interpolation
-sdo_img = np.flip(np.array(cusp_submap._data), axis=0)
+sdo_img = np.array(cusp_submap._data)
 sdo_img_rot = rotate(sdo_img, -19, reshape=False)
 #sdo_img_mag = clipped_zoom(sdo_img_rot, 1.5)
 
@@ -98,19 +108,41 @@ fname = os.getcwd() + "/" + str(instr) + "_" + str(channel) + '_' + timestep
 
 aia_synthetic = synt_img(subs_ds, instr, channel)
 synth_plot_settings = {'resolution': sdo_img_rot.shape[0]}
-synth_view_settings = {'normal_vector': [0.35, 0.0, 0.836],
+synth_view_settings = {'normal_vector': [0.25, 0.0, 0.866],
                        'north_vector': [0.0, 1.0, 0.0]}
+
 aia_synthetic.proj_and_imag(plot_settings=synth_plot_settings, view_settings=synth_view_settings)
 
+# import yt
+# #aia_synthetic.make_filter_image_field()
+# ds = yt.load(downs_file_path, units_override=units_override, hint='AthenaDataset')
+# imaging_model = uv.UVModel("temperature", "density", channel)
+# imaging_model.make_intensity_fields(ds)
+#
+# prji = yt.visualization.volume_rendering.off_axis_projection.off_axis_projection(
+#             ds,
+#             [0.0, 0.5, 0.0],  # center position in code units
+#             normal_vector=synth_view_settings['normal_vector'],  # normal vector (z axis)
+#             width=ds.domain_width[0].value,  # width in code units
+#             resolution=synth_plot_settings['resolution'],  # image resolution
+#             item=('gas', 'aia_filter_band'),  # respective field that is being projected
+#             north_vector=synth_view_settings['north_vector'])
+
 #%%
-synth_imag_rot = np.rot90(np.array(aia_synthetic.image))  # np.rot90(aia_synthetic.image) #rotate(np.array(imag), 90, reshape=False)
+# plt.imshow(prji, norm=colors.LogNorm(vmin=1, vmax=800), origin='lower')
+# plt.savefig('synth_imag.png')
+#%%
+pix2Mm = sdo_img_rot.shape[0]/width_Mm
+
+#%%
+synth_imag_rot = (np.array(aia_synthetic.image))  # np.rot90(aia_synthetic.image) #rotate(np.array(imag), 90, reshape=False)
 
 '''
 Examine synth_image brightness distribution along the line
 '''
 
-zi = ProfilePlot([310, 300], [310, 5], 500, sdo_img_rot)
-prof2 = ProfilePlot([535, 236], [100, 236], 500, synth_imag_rot)
+zi = ProfilePlot([310, 280], [310, 130], 500, sdo_img_rot)  # SDO Slit
+prof2 = ProfilePlot([535, 236], [250, 236], 500, synth_imag_rot)  # Synth image slit
 
 '''
 Sample imag
@@ -143,14 +175,28 @@ plt.style.use('fast')
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 gs = fig.add_gridspec(3, 4)
 
-ax1 = fig.add_subplot(gs[:2, :2])
+#%%
+#aia_rotated = cusp_submap.rotate(angle=1 * u.deg)
 
-im = ax1.imshow(sdo_img_rot, norm=colors.LogNorm(vmin=1, vmax=800), cmap=sdoaia131)
+ax1 = fig.add_subplot(gs[:2, :2], projection=cusp_submap)
+cusp_submap.plot(axes=ax1, clip_interval=(1, 99.99)*u.percent)
+#aia_rotated.plot(axes=ax1, clip_interval=(1, 99.9)*u.percent)
+stonyhurst_grid = cusp_submap.draw_grid(axes=ax1, system='stonyhurst')
+#cusp_submap.draw_grid(axes=ax1)
+#aia_map.draw_limb(axes=ax1)
+#%%
+#im = ax1.imshow(sdo_img_rot, norm=colors.LogNorm(vmin=1, vmax=800), cmap=sdoaia131,
+#                origin='lower', extent=[0, width_Mm, 0, height_Mm])
+
+res = sdo_img_rot.shape[0]
+X, Y = np.mgrid[0:width_Mm:complex(0, res), 0:height_Mm:complex(0, res)]
+#im = ax1.imshow(sdo_img, norm=colors.LogNorm(vmin=1, vmax=800), cmap=sdoaia131, origin='lower')
+
 ax1.plot([zi.pix1[0], zi.pix2[0]], [zi.pix1[1], zi.pix2[1]], 'ro-')
 ax1.axis('image')
-ax1.set_title('AIA/SDO 131A 2011-03-07 18:09:09')
-ax1.set_xlabel('X, Pixel')
-ax1.set_ylabel('Y, Pixel')
+ax1.set_title('AIA/SDO 131A 2011-03-07 18:09:09', pad=44)
+ax1.set_xlabel('X, pix')
+ax1.set_ylabel('Y, pix')
 
 ax2 = fig.add_subplot(gs[:2, 2:])
 divider = make_axes_locatable(ax2)
@@ -158,7 +204,11 @@ cax = divider.append_axes('right', size='5%', pad=0.05)
 ax2.set_title('Synthetic image')
 ax2.set_xlabel('X, Pixel')
 ax2.set_ylabel('Y, Pixel')
-ax2.imshow(synth_imag_rot, norm=colors.LogNorm(vmin=1, vmax=800), cmap=sdoaia131)
+
+X, Y = np.mgrid[0:width_Mm*pix2Mm:complex(0, res+1), 0:height_Mm*pix2Mm:complex(0, res+1)]
+#im = ax2.imshow(synth_imag_rot, norm=colors.LogNorm(vmin=1, vmax=800), cmap=sdoaia131, origin='lower')
+im = ax2.pcolor(X, Y, synth_imag_rot, norm=colors.LogNorm(vmin=1, vmax=800), cmap=sdoaia131)
+ax2.set_aspect('equal')
 ax2.plot([prof2.pix1[1], prof2.pix2[1]], [prof2.pix1[0], prof2.pix2[0]], 'ro-')
 cbar = fig.colorbar(im, cax=cax, orientation='vertical')
 cbar.set_label('DN pixel$^{-1}$ s$^{-1}$', rotation=270, labelpad=13)
@@ -173,5 +223,6 @@ ax3.set_yscale('log')
 
 #plt.tight_layout()
 
-plt.savefig('cut_side_4.png')
+plt.ioff()
+plt.savefig('cut_side_4_0.png')
 plt.close()
