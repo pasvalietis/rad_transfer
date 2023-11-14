024c0f5237d2310b86dbd8ab278e60d5b11f440c
Ivan Oparin <ivandmit@gmail.com>
2023-11-13 20:30:41 -0500

    Fixed imports order

--- tests/read_timeseries.py
+++ tests/read_timeseries.py
@@ -1,5 +1,10 @@
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
-import yt
+import matplotlib.colors as colors
+import pickle
+
+# import yt
 import os, sys, re
 import glob  # to load specific timeframes
 
@@ -17,11 +22,12 @@ import warnings
 warnings.filterwarnings("ignore")
 
 sys.path.insert(0, '/home/ivan/Study/Astro/solar')
-from rad_transfer.utils.proj_imag import SyntheticFilterImage as synt_img
-from visualization.colormaps import color_tables
+# from rad_transfer.utils.proj_imag import SyntheticFilterImage as synt_img
+# from rad_transfer.visualization.colormaps import color_tables
 
-yt.enable_parallelism()
+# matplotlib.use('Qt5Agg')
 
+# yt.enable_parallelism()
 
 def read_dataset(ds_dir):
     ds_lst = glob.glob(ds_dir+"/*.h5") #[1][0-9].h5")
@@ -71,17 +77,17 @@ def download_maps():
     bottom_left = SkyCoord(Tx=-500*u.arcsec, Ty=200*u.arcsec, obstime=start_time, observer="earth", frame="helioprojective")
     top_right = SkyCoord(Tx=-150*u.arcsec, Ty=550*u.arcsec, obstime=start_time, observer="earth", frame="helioprojective")
 
-    end_time = Time('2011-03-07 16:25:00')
-    trange = a.Time(start_time, end_time)
+    #end_time = Time('2011-03-07 16:25:00')
+    #trange = a.Time(start_time, end_time)
 
     cutout = a.jsoc.Cutout(bottom_left, top_right=top_right, tracking=True)
 
     jsoc_email = "ido4@njit.edu"  # Credentials for JSOC database
 
     query = Fido.search(
-        a.Time(start_time - 0*u.h, start_time + 2*u.h),
+        a.Time(start_time - 0*u.h, start_time + 6*u.h),
         a.Wavelength(131*u.angstrom),
-        a.Sample(2.5*u.min),
+        a.Sample(12*u.s),
         a.jsoc.Series.aia_lev1_euv_12s,
         a.jsoc.Notify(jsoc_email),
         a.jsoc.Segment.image,
@@ -94,18 +100,21 @@ def download_maps():
     files.sort()
 
 
-def write_slit_profile(Map, line_coords, intensities, timesteps, start_time, timescale=None):
+def write_slit_profile(Map, line_coords, intensities, timesteps, start_time, timescale=None, synth=False):
 
     intensity_coords_ = sunpy.map.pixelate_coord_path(Map, line_coords)
     intensity_ = sunpy.map.sample_at_coords(Map, intensity_coords_)
     angular_separation_ = intensity_coords_.separation(intensity_coords_[0]).to(u.arcsec)
-    intensities.append(intensity_[:242])
+    if synth:
+        intensities.append(intensity_)
+    else:
+        intensities.append(intensity_[:241])
     timesteps.append((Map.date - start_time).to_value('s'))
 
     return angular_separation_
-    #%%
+        #%%
 
-def process_aia_maps(obs_data_path, start_time):
+    # def process_aia_maps(obs_data_path, start_time, running_diff=False):
     intensities_AIA = []
     timesteps_AIA = []
     maps_list = []
@@ -115,30 +124,63 @@ def process_aia_maps(obs_data_path, start_time):
         download_maps()
 
     mapsequence = sunpy.map.Map(obs_data_path + '/aia.lev1_euv_12s.2011-03-07T*.fits', sequence=True)
+
+    #m_seq_base = sunpy.map.Map([m - m_seq[0].quantity for m in m_seq[1:]], sequence=True)
+    #%%
+    running_diff = False
+    if running_diff:
+        m_seq_running = sunpy.map.Map(
+            [m/prev_m.quantity for m, prev_m in zip(mapsequence[4:], mapsequence[:-4])],
+            sequence=True
+        )
+        map_seq = m_seq_running
+    else:
+        map_seq = mapsequence
+    #%%
+    import aiapy.data.sample as sample_data
+    from aiapy.calibrate import normalize_exposure, register, update_pointing
+
+    #%%
+
     line_coords_ = SkyCoord([-335, -380], [380, 480], unit=(u.arcsec, u.arcsec), frame=mapsequence[0].coordinate_frame)
 
     # RETURN AIA MAP SLIT PROFILES
-    for map in mapsequence:
+    idx = 0
+    plt.ioff()
+    #%%
+    for map in map_seq:
+
+        #fig = plt.figure()
+        #ax = fig.add_subplot(projection=map)
+        #ax.imshow(map.data,
+        #          origin="lower", norm=colors.Normalize(vmin=-200, vmax=200), cmap='Greys_r')
+        #plt.savefig('aiamaps/map_'+str(idx)+'.png')
+        #idx += 1
+        #print(map._date_obs.value)
         angular_separation_ = write_slit_profile(map, line_coords_, intensities_AIA, timesteps_AIA, start_time)
 
+    #plt.close()
     timesteps = np.transpose(np.array(timesteps_AIA))
-
+    #%%
     xs = np.append(timesteps - np.diff(timesteps)[0] / 2., timesteps[-1] + np.diff(timesteps)[0] / 2.)[:]
     ys = np.append(angular_separation_ - np.diff(angular_separation_)[0] / 2.,
-                   angular_separation_[-1] + np.diff(angular_separation_)[0] / 2.)[:-1]
+                   angular_separation_[-1] + np.diff(angular_separation_)[0] / 2.)[:-2]
     X, Y = np.meshgrid(np.array(xs), np.array(ys))
     Z = np.vstack(intensities_AIA).transpose()
-
+    #%%
     cmap = color_tables.aia_color_table(int(131) * u.angstrom)
-    plt.pcolormesh(X, Y, Z, shading='flat', cmap=cmap)
+    #vmin = 0. #np.min(Z)
+    #vmax = 600. #np.max(Z)
+    plt.pcolormesh(X, Y, Z, shading='flat', cmap='cubehelix',  norm=colors.LogNorm(vmin=0.2, vmax=3))# norm=colors.Normalize(vmin=0.8, vmax=1.4)) #cmap=cmap) #, norm=colors.Normalize(vmin=0.5, vmax=2) cubehelix
     plt.xlabel('timestep, s')
     plt.ylabel('Angular distance, arcsec')
     plt.title('start_time: ' + start_time.value)
+    plt.colorbar()
 
-    plt.savefig('aia_obs_time_distance.eps')
-
-    return line_coords_
-
+    plt.savefig('aia_obs_time_distance_diff_ratio_log.png', dpi=300)
+    plt.close()
+    return [line_coords_, mapsequence]
+#%%
 #%%
 def process_synth_maps(ds_dir, start_time, line_coords):
     ts = read_dataset(ds_dir)
@@ -149,7 +191,8 @@ def process_synth_maps(ds_dir, start_time, line_coords):
 
     for sto, ds in ts.piter(storage=storage):
         synth_map = gen_map_from_timeseries(ds, start_time, timescale=109.8)
-        angular_separation_ = write_slit_profile(synth_map, line_coords, intensities_SYNTH, timesteps_SYNTH, start_time)
+        angular_separation_ = write_slit_profile(synth_map, line_coords, intensities_SYNTH, timesteps_SYNTH,
+                                                 start_time, synth=True)
 
     xs = np.append(timesteps_SYNTH - np.diff(timesteps_SYNTH)[0] / 2.,
                    timesteps_SYNTH[-1] + np.diff(timesteps_SYNTH)[0] / 2.)
@@ -162,21 +205,54 @@ def process_synth_maps(ds_dir, start_time, line_coords):
     plt.pcolormesh(X, Y, Z.transpose(), shading='flat', cmap=cmap)
     plt.xlabel('timestep, s')
     plt.ylabel('Angular distance, arcsec')
-    plt.legend()
+    #plt.legend()
     plt.savefig('time_distance.eps')
-
+    plt.close()
     return
 
 #%%
 
 if __name__ == '__main__':
+    # Attempt to call stackplotX to plot time-distance plots
+    import stackplotX as stp
+    import matplotlib.colors as colors
 
     ds_dir = '/media/ivan/TOSHIBA EXT/subs'
-    obs_data_path = '/home/ivan/sunpy/data'  # '/media/ivan/TOSHIBA EXT/aia_img'
-    ts = read_dataset(ds_dir)
+    obs_data_path = '/media/ivan/TOSHIBA EXT/aia_img/maps_part/'
+    #'/home/ivan/sunpy/data'  # '/media/ivan/TOSHIBA EXT/aia_img' # '/media/ivan/TOSHIBA EXT/aia_img/low_res'#
+    # ts = read_dataset(ds_dir)
     start_time = Time('2011-03-07T13:45:26', scale='utc', format='isot')
 
-    line_coords = process_aia_maps(obs_data_path, start_time)
+    #line_coords = process_aia_maps(obs_data_path, start_time, running_diff=False)[0]
+#%%
+    #with open('/media/ivan/TOSHIBA EXT/aia_img/buffer/aia_map_seq.p', 'rb') as f:
+        # The protocol version used is detected automatically, so we do not
+        # have to specify it.
+        #mapsequence = pickle.load(f)
+        # pickle.dump(mapsequence, f)
+    #filehandler = open("/media/ivan/TOSHIBA EXT/aia_img/map_seq.obj", "wb")
+    #pickle.dump(mapsequence, filehandler)
+    #filehandler.close()
+#%%
+    #file = open("/media/ivan/TOSHIBA EXT/aia_img/map_seq.obj", 'rb')
+    #object_file = pickle.load(file)
+    #file.close()
     #%%
-    #TODO: FIX upper boundary limit for intensity array (242 max)
-    process_synth_maps(ds_dir, start_time, line_coords)
+    mapsequence = sunpy.map.Map(obs_data_path + '/aia.lev1_euv_12s.2011-03-07T*.fits', sequence=True)
+    # #%%
+    sequence = mapsequence # process_aia_maps(obs_data_path, start_time)[1]
+    st = stp.Stackplot(sequence)
+    #%%
+    st.mapseq_mkdiff(mode='rratio', dt=12.)
+    st.plot_mapseq(diff=True, norm=colors.Normalize(vmin=0.1, vmax=2.4)) #norm=colors.LogNorm(vmin=1e1, vmax=8e2))
+    #%%
+    #st.cutslitbd.update()
+    #%%
+    #st.plot_stackplot(norm=colors.Normalize(vmin=0.8, vmax=1.4), cmap='Greys', uni_cm=True)
+    #%%
+    #st.stackplt_tofile('stack_plot_output.p')
+
+    # #%%
+    # #TODO: FIX upper boundary limit for intensity array (242 max)
+    # #process_synth_maps(ds_dir, start_time, line_coords)
+
