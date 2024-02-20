bc1b3c8fed87e0209f05ea6c4469571788c7b5b9
Ivan Oparin <ivandmit@gmail.com>
2024-02-19 19:38:25 -0500

    Revert "fixed imports"
    This reverts commit f1a5a4a16e2245c6645e24dce0f4905ad55b69b3.

--- utils/proj_imag.py
+++ utils/proj_imag.py
@@ -8,6 +8,9 @@ import astropy.units as u
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 
+from emission_models import uv, xrt, xray_bremsstrahlung
+from visualization.colormaps import color_tables
+
 # Importing sunpy dependencies for a synthetic map
 # See creating custom maps: https://docs.sunpy.org/en/stable/how_to/create_custom_map.html
 import sunpy.map
@@ -16,11 +19,7 @@ import textwrap
 from astropy.coordinates import SkyCoord
 from sunpy.coordinates import frames
 from sunpy.map.header_helper import make_fitswcs_header
-# from sunpy.coordinates.sun import _radius_from_angular_radius
-
-sys.path.insert(0, '/home/ivan/Study/Astro/solar')
-from rad_transfer.emission_models import uv, xrt, xray_bremsstrahlung
-from rad_transfer.visualization.colormaps import color_tables
+from sunpy.coordinates.sun import _radius_from_angular_radius
 
 class SyntheticImage():
 
