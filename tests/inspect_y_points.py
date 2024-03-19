c4bf6a04ed1f4c4df75cd57d0b13a8d6d7962203
Ivan Oparin <ivandmit@gmail.com>
2024-03-19 10:57:24 -0400

    Using algorithm to identify locations of y points

--- tests/inspect_y_points.py
+++ tests/inspect_y_points.py
@@ -13,6 +13,10 @@ from yt.visualization.image_writer import write_image
 from current_density import _current_density
 sys.path.insert(0, '/home/ivan/Study/Astro/solar')
 
+def detect_sharp_slope(arr):
+
+    pass
+
 if __name__ == '__main__':
 
     ds_dir = '/media/ivan/TOSHIBA EXT/subs'
@@ -35,51 +39,112 @@ if __name__ == '__main__':
         force_override=True
         # validators=[ValidateParameter(["center", "bulk_velocity"])],
     )
+
 #%%
 # make a cut along x = 0
-    nslices = 1
+    nslices = 8
+    axes = 'xyz'
+    nax = 2
+    axis = axes[nax]
 
-    x_coord = 0.
-    slc = rad_buffer_obj.slice("x", x_coord)  #, 'current_density')
-    slc_frb = slc.to_frb((140.0, "Mm"), 512)
+    coord_range = np.linspace(rad_buffer_obj.domain_left_edge[nax].value,
+                              rad_buffer_obj.domain_right_edge[nax].value,
+                              nslices)
 
-    jz_arr = (slc_frb["gas", "current_density"].d)
-    plt.imshow(np.rot90(jz_arr), origin='upper', vmin=8e-7, vmax=1e-5, cmap='RdBu_r')
+    for i in range(nslices):
 
-    plt.colorbar(label="Current density", orientation="vertical")
-    plt.title("Current density $j_z$, "+"x="+f'{x_coord:.4f}')
-    # plt.show()
-    plt.savefig('jz_slices/current_density_slice_x_'+f'{x_coord:.4f}'+'.png')
-    plt.close()
+        coord = coord_range[i]
+        #coord = -0.0833
+        if i == 0 or i == nslices - 1:
+            coord = np.trunc(coord*1e3)/1e3
+
+        slc = rad_buffer_obj.slice(axis, coord)  #, 'current_density')
+        slc_frb = slc.to_frb((140.0, "Mm"), 512)
+
+        jz_arr = slc_frb["gas", "current_density"].d
+        # cs_profile =
+        if axis == 'z':
+            z_coord = coord
+        else:
+            z_coord = None
+#%%
+        cs_loc = rad_buffer_obj.ray([-0.05, 0.98, z_coord],
+                                    [0.05, 0.98, z_coord])  # Identify x coordinate of the current sheet
+        cs_loc_profile = cs_loc[('gas', 'current_density')].value
+        cs_max_x_coord = cs_loc.argmax(('gas', 'current_density'))[0].value
+
+        cs_ray = rad_buffer_obj.ray([cs_max_x_coord, 0.0, z_coord], [cs_max_x_coord, 1.0, z_coord])
+        cs_profile = cs_ray[('gas', 'current_density')].value
+        cs_profile_coords = cs_ray.fcoords.value[:, 1]  # cs_profile ray coordinates along y
+        # plt.plot(np.linspace(0, 1, cs_profile.shape[0]), cs_profile)
+
+#%%
+        fig, ax = plt.subplots(1, 1, figsize=(8.0, 7.0))
+        im_cmap = 'BuPu'  #RdBu_r
+        if axis == 'x':
+            im = ax.imshow(np.rot90(jz_arr), origin='upper', vmin=8e-7, vmax=1e-5, cmap=im_cmap)
+        elif axis == 'z':
+            im = ax.imshow(jz_arr, origin='lower', vmin=8e-7, vmax=1e-5, cmap=im_cmap, extent=(-0.5, 0.5, 0, 1.0))
+        else:
+            im = None
+
+        from mpl_toolkits.axes_grid1 import make_axes_locatable
+        divider = make_axes_locatable(ax)
+        cax1 = divider.append_axes("right", size="5%", pad=0.05)
+        fig.colorbar(im, cax=cax1, label="Current density $j_z$", orientation="vertical")
+        ax.spines['left'].set_color('none')
+        ax.set_yticks([])
+        ax.axvline(x=cs_max_x_coord, color='magenta', alpha=0.5,  linestyle='--', label='axvline - full height')
+
+        ax.set_title("Current density $j_z$, "+axis+"="+f'{coord:.4f}')
+
+        # divider2 = make_axes_locatable(ax)
+        ax11 = divider.append_axes("left", size="40%", pad=0.30)
+        ax11.plot(cs_profile, np.linspace(0, 1, cs_profile.shape[0]), linewidth=0.65, color='indigo')
+        #ax11.plot(np.gradient(cs_profile, np.linspace(0, 1, cs_profile.shape[0])),
+        #          np.linspace(0, 1, cs_profile.shape[0]))
+        ax11.set_ylim(0, 1)
+        ax11.set_xlim(0, 2e-5)
+        ax11.set_ylabel('y, code length')
+        ax11.set_xlabel('$j_z$')
+
+        ax12 = ax11.twiny()
+        color = 'tab:blue'
+        ax12.set_xlabel('$dj_z/dy$', color=color)  # we already handled the x-label with ax1
+        y_cut = np.linspace(0, 1, cs_profile.shape[0])
+        cs_der_y = np.gradient(cs_profile, y_cut)
+        ax12.plot(cs_der_y, y_cut, color=color, linewidth=0.65, alpha=0.75)
+        ax12.tick_params(axis='y', labelcolor=color)
+        ax12.set_ylim(0, 1)
+        ax12.set_xlim(0.25*cs_der_y.min(), 0.25*cs_der_y.max())
+        '''
+        Identifying location of y-point from the maximum of the first derivative
+        '''
+        crd_idx = cs_der_y[np.where((cs_profile_coords > 0.75) & (cs_profile_coords < 0.95))].argmax()
+        yp_ycoord = cs_profile_coords[np.where((cs_profile_coords > 0.75) & (cs_profile_coords < 0.95))][crd_idx]
+        ax.axhline(y=yp_ycoord, color='magenta', alpha=0.2)
+        # Plot the location of the y point
+        ax.scatter([cs_max_x_coord], [yp_ycoord], marker='x', color='magenta')
+        #ax12.set_yticks([])
+        # plt.show()
+
+        '''
+        Alternative method: find where j_z is about one half of initial value on top of the domain (y~0.95)
+        '''
+        init_idx = np.argmin(abs(cs_profile_coords - 0.96))
+        half_idx = np.argmin(abs(cs_profile - 0.5*cs_profile[init_idx]))
+
+        yp_ycoord = cs_profile_coords[half_idx]
+
+        ax.scatter([cs_max_x_coord], [yp_ycoord], marker='x', color='red')
 
 
-    # nslices = 100
-    # for i in range(nslices):
-    #
-    #     # z from -0.25 to 0.25
-    #     z_coord = -0.25 + (i * 0.5 / nslices)
-    #     slc = rad_buffer_obj.slice("z", z_coord)  #, 'current_density')
-    #     slc_frb = slc.to_frb((140.0, "Mm"), 512)
-    #     #write_image(np.log10(slc_frb[("gas", "current_density")]), "current_density.png")
-    #
-    #     # export current_density array frb["gas", "current_density"].d
-    #     jz_arr = (slc_frb["gas", "current_density"].d)
-    #
-    #     plt.imshow(jz_arr, origin='lower', vmin=8e-7, vmax=1e-5, cmap='RdBu_r')
-    #     # plt.imshow(jz_arr, origin='lower', norm=colors.LogNorm(vmin=jz_arr.min(), vmax=jz_arr.max()))
-    #     plt.colorbar(label="Current density", orientation="vertical")
-    #     plt.title("Current density $j_z$, "+"z="+str(z_coord))
-    #     # plt.show()
-    #     plt.savefig('jz_slices/current_density_slice_z_'+f'{z_coord:.4f}'+'.png')
-    #     plt.close()
-    #     #slc = yt.SlicePlot(rad_buffer_obj, "z", ("gas", "current_density"))
-    #     #slc.save()
-    #     #p = yt.ProjectionPlot(rad_buffer_obj, 'z', ("gas", "current_density"))
+        ax22 = divider.append_axes("bottom", size="25%", pad=0.5)
+        ax22.plot(np.linspace(-0.05, 0.05, cs_loc_profile.shape[0]), cs_loc_profile, linewidth=0.65, color='magenta')
+        ax22.axvline(x=cs_max_x_coord, color='red', alpha=0.95, linestyle='-', label='axvline - full height')
+        ax22.set_ylabel('$j_z$')
+        ax22.set_xlabel('x, code length')
 
-
-
-
-
-
-
-
+        plt.savefig('cur_dens_slices/current_density_slice_'+axis+'_'+f'{coord:.4f}'+'.png')
+        plt.close()
+        print(i)
