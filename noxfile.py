863aa23497c7b3975f17ef5a07ccbbb8848cda97
Nick Murphy <namurphy@cfa.harvard.edu>
2025-08-28 18:47:34 -0400

    Add docstring to noxfile.py
    I based this off of PlasmaPy's noxfile.py.

--- noxfile.py
+++ noxfile.py
@@ -1,9 +1,22 @@
-import nox
+"""
+Configuration file for Nox.
 
+Nox is a task automation tool that can be used to run tests, build
+documentation, and perform other checks. Nox sessions are defined in
+`noxfile.py`.
 
-supported_python_versions = ("3.12", "3.13")
+Running `nox` without arguments will run tests with the version of
+Python that `nox` is installed under.
 
-@nox.session
-def tests(session: nox.Session):
-    session.install(".[tests]")
-    session.run("pytest", "--tb=short")
+To invoke a nox session, enter the top-level directory of this
+repository and run
+
+    nox -s "<session>"
+
+where <session> is replaced with the name of the session. The quotes are
+necessary if the session name contains special characters.
+
+To list available sessions, run `nox -l`.
+
+Nox documentation: https://nox.thea.codes
+"""
