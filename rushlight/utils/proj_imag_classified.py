41d69313a36b4394c0815a48f7fdb1329aa1787e
Sabastian Carlos Fernandes <sabastianf531@gmail.com>
2025-09-17 11:44:17 -0400

    Dummy Dataset Autocreate

--- rushlight/utils/proj_imag_classified.py
+++ rushlight/utils/proj_imag_classified.py
@@ -48,8 +48,6 @@ class SyntheticImage(ABC):
     """
     ## Parent class for generating synthetic images using rad_transfer
     
-    .. _reference:
-
     Contains all of the base functionality necessary for:
      * **Setting the positions** of the observer and of the feature, relative to the center of the sun
      * **Orienting and projecting** a 3D dataset according to specified lines of sight
@@ -62,7 +60,7 @@ class SyntheticImage(ABC):
 
     def __init__(self, dataset = None, smap_path: str=None, smap=None, **kwargs):
         """
-        ### Constructor for the synthetic image class. See :ref:`reference`
+        ### Constructor for the synthetic image class.
         
         Ensures that all necessary components are loaded / interpreted on call.
         User should provide a 3D dataset and a synthetic map path / object, 
@@ -130,29 +128,10 @@ class SyntheticImage(ABC):
                               'north_vector': self.northvector}
 
         # Initialize the 3D MHD file to be used for synthetic image
-        shen_datacube = config.SIMULATIONS['DATASET']   # Default datacube TODO make this generic
-        if dataset:
-            if isinstance(dataset, YTRegion):
-                self.box = dataset
-                self.data = self.box.ds
-                self.domain_width = np.abs(self.box.right_edge - self.box.left_edge).in_units('cm').to_astropy()
-            else:
-                if isinstance(dataset, str):
-                    self.data = yt.load(dataset)
-                    self.box = self.data
-                else:
-                    try:
-                        dataset.field_list
-                        self.data = dataset
-                        self.box = self.data
-                    except:
-                        print('Invalid datacube provided! Using default datacube... \n')
-                        self.data = yt.load(shen_datacube)
-                        self.box = self.data
-                self.domain_width = np.abs(self.data.domain_right_edge - self.data.domain_left_edge).in_units('cm').to_astropy()
-        else:
-            print('No datacube provided! Using default datacube... \n')
-            self.data = yt.load(shen_datacube)
+        ds = Dataset(dataset)
+        self.box = ds.box
+        self.data = ds.data
+        self.domain_width = ds.domain_width
 
         # Determine synthetic observation time with respect to observation time
         self.timescale = kwargs.get('timescale', 109.8)
@@ -858,7 +837,6 @@ class ReferenceImage(ABC, MapFactory):
         :param ref_img_path: Path to the reference image .fits file, defaults to None
         :type ref_img_path: str, optional
         """
-        reference_image = None
 
         if ref_img_path:
             m = sunpy.map.Map(ref_img_path)
@@ -914,3 +892,116 @@ class AIAReferenceImage(ReferenceImage):
 
     def __init__(self, ref_img_path):
         super().__init__(ref_img_path)
+
+###############################################
+# 3D Dataset
+
+class Dataset(ABC):
+
+    def __init__(self, dataset = None):
+
+        if not dataset:
+            # Default values for bbox dimensions (code units)
+            fract = 1
+            dim0 = int(193 / fract)
+            dim1 = int(192 / fract)
+            dim2 = int(96 / fract)
+
+            # Store dimensions in a list. Example: [depth, height, width].
+            dims = [dim0, dim1, dim2]
+
+            # Define the range for the logarithmic distribution.
+            mi = 4
+            ma = 8
+
+            # Create a logarithmically spaced array based on the first dimension in 'dims'.
+            arr_range = np.logspace(mi, ma, dims[0])
+
+            # Initialize an empty array with the specified arbitrary dimensions.
+            arr = np.empty(tuple(dims))
+
+            # Fill the array. The 'fill' value varies along the first dimension (axis 0).
+            # The `np.full` call creates a 2D array that matches the remaining dimensions
+            # (dims[1] and dims[2]).
+            for i in range(0, dims[0]):
+                fill = arr_range[i]
+                arr[i] = np.full((dims[1], dims[2]), fill)
+
+            # Define scaling factor for creating "chunks" in the dataset.
+            fract = 3
+            ch0 = int(dims[0] / fract)
+            ch1 = int(dims[1] / fract)
+            ch2 = int(dims[2] / fract)
+
+            # Cut out some chunks from the array to create a more visually distinctive dataset.
+            # These chunks are set to the maximum value in `arr_range`.
+            arr[dim0 - ch0 : dim0, dim1 - ch1 : dim1, dim2 - ch2 : dim2] = arr_range.max()
+            arr[0:ch0, 0:ch1, 0:ch2] = arr_range.max()
+
+            # Rotate the array to reorient the data.
+            arr = np.rot90(arr, k=-1, axes=(0, 1))
+            arr = np.rot90(arr, k=1, axes=(0, 2))
+
+            # %%
+            # Create a yt object from the NumPy array.
+
+            T_arr = arr         # Assign the generated array to represent temperature.
+            D_arr = arr * 2     # Create a density array by scaling the temperature array.
+
+            # Define the data dictionary with field names, arrays, and their units.
+            # Units can be specified using CGS (centimeter-gram-second) units.
+            data = {
+                "temperature": (T_arr, "K"),
+                "density": (D_arr, "g/cm**3"),
+            }
+
+            # Define the spatial ranges for the bounding box.
+            xrange = np.array([-0.5, 0.5])  # Length = 1 unit.
+            yrange = np.array([0, 1])  # Length = 1 unit.
+            zrange = np.array([-0.25, 0.25])  # Length = 0.5 units.
+
+            # Combine ranges to form the bounding box.
+            bbox = np.array([xrange, yrange, zrange])
+
+            # Load the uniform grid data into a yt dataset object.
+            # The unit length (1.5e10 cm = 1.5e5 km) is specified.
+            ds = yt.load_uniform_grid(
+                data,
+                arr.shape,
+                1.5e10,  # 1.5e10 cm = 1.5e5 km (unit length)
+                bbox=bbox,
+            )
+
+            # Create a covering grid from the dataset at level 0.
+            cg = ds.covering_grid(
+                level=0,
+                left_edge=[xrange[0], yrange[0], zrange[0]],
+                dims=ds.domain_dimensions,
+            )
+
+            # Save the covering grid as an HDF5 dataset.
+            # The `fields` argument specifies the fields to be saved.
+            fname = "test.h5"
+            fn = cg.save_as_dataset(filename=fname, fields=[("gas", "temperature"), ("stream", "density")])
+            print('Saved dummy dataset to: ' + fname)
+
+            dataset = yt.load("test.h5")
+        
+        if isinstance(dataset, YTRegion):
+            self.box = dataset
+            self.data = self.box.dataset
+            self.domain_width = np.abs(self.box.right_edge - self.box.left_edge).in_units('cm').to_astropy()
+        else:
+            if isinstance(dataset, str):
+                self.data = yt.load(dataset)
+                self.box = self.data
+            else:
+                try:
+                    dataset.field_list
+                    self.data = dataset
+                    self.box = self.data
+                except:
+                    print('Invalid datacube provided! Using default datacube... \n')
+                    self.data = yt.load(shen_datacube)
+                    self.box = self.data
+            self.domain_width = np.abs(self.data.domain_right_edge - self.data.domain_left_edge).in_units('cm').to_astropy()
